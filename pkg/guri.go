ce9bc9cd6a10a2958f87785053f7d7ec010fc1ff
nodata <nodata.eng@gmail.com>
2021-09-23 18:22:46 +0900

    📝 code comment change.

--- pkg/guri.go
+++ pkg/guri.go
@@ -32,26 +32,29 @@ func New() error {
 	for scanner.Scan() {
 		t.url = append(t.url, scanner.Text())
 	}
-	//request url
 	for i, u := range t.url {
+		//request url
 		resp, err := http.Get(u)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
+
+		//byte to image
 		imgByte, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 		img, _, _ := image.Decode(bytes.NewReader(imgByte))
+
+		//output image
 		file, err := os.Create("./img/" + strconv.Itoa(i) + ".png")
+		defer file.Close()
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-		defer file.Close()
-
 		err = png.Encode(file, img)
 		if err != nil {
 			log.Println(err)
@@ -59,10 +62,6 @@ func New() error {
 		}
 
 	}
-
-	//byte to image
-
-	//output image
 
 	return nil
 }
